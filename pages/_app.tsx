5c20c400a2e7b7480bd8fe1836b7d1f06d158fd1
Annie Sawers <ann.sawers@duke.edu>
2023-01-27 01:48:53 -0500

    i added some home page stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import '../styles/design_tokens.css'
 import type { AppProps } from 'next/app'
 import { Nunito } from '@next/font/google'
+
+
+
 
 const nunito = Nunito({
   weight: ['400', '500', '700'],
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/home/Home.module.css';
+import { Panel, Placeholder, Progress } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+import Header from '../components/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +17,55 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Test
+
+      <div className={styles.Background}>
+        
+
+      <div className={styles.Header}>
+
+        <div className={styles.Title}>
+          <h2 style={{fontWeight:600}}>CS 101: Spring 2023</h2>
+          <h3 style={{ marginTop: "-15px"}}>This is a webpage for checking and grading your APTs</h3>
+        </div>
+
+        <h3>My Grades</h3>
+        <h3>How Do APTs Work?</h3>
+        <h3>Get Help</h3>
+
+      </div>
+
+      <div className={styles.APTGroup}>
+
+            <Panel style={{fontWeight:600, borderColor:"var(--blue)", borderWidth:"2px"}} header={
+              <div style={{display:"flex", flexDirection:"row"}}>
+                <div style={{display: "flex", backgroundColor:"var(--blue)", width:"30px", margin:"-20px"}}></div>
+                <div style={{display:"flex", flexDirection:"column", marginLeft:"25px", width:"100%"}}>
+                  <h2 style={{marginLeft:"10px"}}>APT 0</h2>
+                  <h3 style={{marginLeft:"10px", marginTop:"-15px"}}>
+                    Due: 05/12/2000   |   Complete all 6
+                  </h3>
+                  <Progress.Line percent={80} showInfo={false} strokeColor= "var(--blue)" />
+                </div>
+              </div>
+
+            } collapsible bordered>
+
+
+              <h2>Due: 05/12/2000   |   Complete all 6</h2>
+             
+              <Placeholder.Paragraph />
+            </Panel>
+
+            
+
+      </div>
+
+
+            
+      </div>
+      
+
+      
     </>
   )
 }
